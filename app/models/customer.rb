839b45bcfe280bd58035f39b1f53398034a28b96
Wilson <imweesheng@gmail.com>
2023-12-31 09:20:51 +0800

    Setup has_many orders with dependent option for customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,5 +4,7 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :orders, dependent: :destroy
+
   validates :name, presence: true
 end
