0522e925222d1ff874cae47f4235aae49f943818
Wilson <imweesheng@gmail.com>
2023-12-31 09:21:48 +0800

    Setup has_many products with through option for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items, dependent: :destroy
+  has_many :products, through: :order_items
 
   validates :status, presence: true
   validates :status, inclusion: { in: ['pending', 'paid'], message: "%{value} is not a valid status" }, if: -> { status.present? }
